d9f8c52270febcb438f909ff7d59e6485fd25f7d
muhammadtohir01 <nabiyevmuhammadtohir@gmail.com>
2023-02-08 22:12:12 +0500

    salom

--- app.py
+++ app.py
@@ -4,16 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1> Hello, Muhammadtohir!</>'
-
-@app.route('/index')
-@app.route('/home')
-def home():
-    return '<h1> UY!</h1>'
-@app.route('/about')
-def about():
-    return "<h1> O'qish </h1>" 
-# This will run the app on http://localhost:5000
+    return """jhgvilgv;kjghiu"""
 if __name__ == '__main__':
     # Run the app in local network
     app.run()
